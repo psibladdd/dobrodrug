c235c1514362f810c77103f7fdf1ea7b20ddeca8
Dima <151432491+psibladdd@users.noreply.github.com>
2024-08-24 16:09:29 +0300

    Add files via upload

--- бот блин/main.py
+++ бот блин/main.py
@@ -4,12 +4,12 @@ import time
 import random
 
 from telegram import *
-from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes, ConversationHandler
+from telegram.ext import *
 import json
 
 logging.basicConfig(level=logging.INFO)
 
-SELECT_USER, ENTER_BALANCE = range(2)
+SELECT_USER, ENTER_BALANCE, SEND_MESSAGE = range(3)
 TOKEN = '7491056485:AAEOEEi60LJCv6lj1meW7Gika0nRmSuh1vM'
 conn = sqlite3.connect('users.db', check_same_thread=False)
 cursor = conn.cursor()
@@ -42,7 +42,7 @@ async def handle_dice(update: Update, context: ContextTypes.DEFAULT_TYPE) -> Non
                                            message_thread_id=4)
             await context.bot.delete_message(chat_id=update.effective_chat.id, message_id=update.message.message_id)
             return
-        time.sleep(2.5)
+        time.sleep(2)
         if dice.emoji == '🎲':  # Кубик
             if dice.value == 1:
                 new_balance = current_balance - 50
@@ -144,10 +144,10 @@ async def handle_dice(update: Update, context: ContextTypes.DEFAULT_TYPE) -> Non
             if dice.value == 24 or dice.value == 25 or dice.value == 26 or dice.value == 18 or dice.value == 30 or dice.value == 38  or dice.value == 54:
                 new_balance = current_balance + 5
                 mess = f'Как вкусно... \n @{user_name} получает 5 очков! \n Баланс @{user_name}: {new_balance}'
-            elif dice.value == 21 or dice.value == 35 or dice.value == 27 or dice.value == 11 or dice.value == 47 or dice.value == 39 or dice.value == 42 or dice.value == 59 or dice.value == 41:
+            elif dice.value == 21 or dice.value == 35 or dice.value == 44 or dice.value == 27 or dice.value == 11 or dice.value == 47 or dice.value == 39 or dice.value == 42 or dice.value == 59 or dice.value == 41:
                 new_balance = current_balance + 7
                 mess = f'А теперь уже кисленько... Зато выйграл! \n @{user_name} получает 7 очков! \n Баланс @{user_name}: {new_balance}'
-            elif dice.value == 17 or dice.value == 13 or dice.value == 3 or dice.value == 4 or dice.value == 9 or dice.value == 5 or dice.value == 2 or dice.value == 49 or dice.value == 33 or dice.value == 18:
+            elif dice.value == 17 or dice.value == 13 or dice.value == 23 or dice.value == 3 or dice.value == 6 or dice.value == 4 or dice.value == 9 or dice.value == 5 or dice.value == 2 or dice.value == 49 or dice.value == 33 or dice.value == 18:
                 new_balance = current_balance + 3
                 mess = f'ВОУ! Это уже приятно! \n @{user_name} получает 3 очка! \n Баланс @{user_name}: {new_balance}'
 
@@ -158,8 +158,8 @@ async def handle_dice(update: Update, context: ContextTypes.DEFAULT_TYPE) -> Non
                 new_balance = current_balance + 50
                 mess = f'Джекпот! Удача на твоей стороне 💸 \n @{user_name} получает 50 очков! \n Баланс @{user_name}: {new_balance}'
             else:
-                new_balance = current_balance - 30
-                mess = f'{dice.value} Удача покинула тебя 😔 \n @{user_name} теряет 30 очков! \n Баланс @{user_name}: {new_balance}'
+                new_balance = current_balance - 20
+                mess = f'{dice.value} Удача покинула тебя 😔 \n @{user_name} теряет 20 очков! \n Баланс @{user_name}: {new_balance}'
 
             cursor.execute('UPDATE users SET balance = ? WHERE id = ?', (new_balance, user_id))
             conn.commit()
@@ -169,7 +169,7 @@ async def handle_dice(update: Update, context: ContextTypes.DEFAULT_TYPE) -> Non
 async def register(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     user_id = update.message.from_user.id
     first_name = update.message.from_user.first_name
-    user_name = update.message.from_user.username
+    user_name = '@'+update.message.from_user.username
 
     cursor.execute('SELECT ID FROM users WHERE ID = ?', (user_id,))
     existing_user = cursor.fetchone()
@@ -181,84 +181,68 @@ async def register(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         conn.commit()
         await context.bot.send_message(chat_id=update.effective_chat.id, text='Вы успешно зарегистрированы!', message_thread_id=4)
 
-async def admin(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def balance(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    print(update.effective_chat.id)
     if update.effective_chat.type != Chat.PRIVATE:
         await context.bot.send_message(chat_id=update.effective_chat.id, text='Эта команда доступна только в личных сообщениях.', message_thread_id=4)
         return
 
     user_name = update.message.from_user.username
-    if user_name == 'hlebnastole' or user_name == 'why_dyrachyo':
-        keyboard = [
-            [KeyboardButton("Изменить баланс пользователя")],
-            [KeyboardButton("Скрытно кого-то послать")],
-            [KeyboardButton("Создать квизз")]
-        ]
-        reply_markup = ReplyKeyboardMarkup(keyboard, one_time_keyboard=True, resize_keyboard=True)
-        await context.bot.send_message(chat_id=update.effective_chat.id, text=f'Привет, {user_name}! Чем могу быть полезен?', reply_markup=reply_markup)
+    if user_name not in ['hlebnastole', 'why_dyrachyo']:
+        await context.bot.send_message(chat_id=update.effective_chat.id, text='У вас нет доступа к этой программе')
         return
-    else:
-        await context.bot.send_message(chat_id=update.effective_chat.id, text='ебать ты лох')
 
+    args = context.args
+    if len(args) != 2:
+        await context.bot.send_message(chat_id=update.effective_chat.id, text='Используйте команду в формате: /balance "username" "число"')
+        return
 
+    target_username = args[0]
+    try:
+        amount = int(args[1])
+    except ValueError:
+        await context.bot.send_message(chat_id=update.effective_chat.id, text='Пожалуйста, введите корректное число.')
+        return
 
-async def button1(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    await context.bot.send_message(chat_id=update.effective_chat.id, text='Введите username пользователя, которому хотите изменить баланс:')
-    return SELECT_USER
-
-async def select_user(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    user_name = update.message.text
-    cursor.execute('SELECT username FROM users WHERE username = ?', (user_name,))
+    cursor.execute('SELECT username FROM users WHERE username = ?', (target_username,))
     existing_user = cursor.fetchone()
 
     if not existing_user:
-        await context.bot.send_message(chat_id=update.effective_chat.id, text=f'Пользователь {user_name} не найден в базе данных.')
-        return ConversationHandler.END
-
-    context.user_data['selected_user'] = user_name
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=f'Введите новый баланс для пользователя {user_name}:')
-    return ENTER_BALANCE
-
-async def enter_balance(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    try:
-        new_balance = int(update.message.text)
-    except ValueError:
-        await context.bot.send_message(chat_id=update.effective_chat.id, text='Пожалуйста, введите корректное число.')
-        return ENTER_BALANCE
-
-    selected_user = context.user_data['selected_user']
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=f'Пользователь {target_username} не найден в базе данных.')
+        return
 
     # Обновление баланса в базе данных
-    cursor.execute('UPDATE users SET balance = ? WHERE username = ?', (new_balance, selected_user))
+    cursor.execute('UPDATE users SET balance = balance + ? WHERE username = ?', (amount, target_username))
     conn.commit()
+    cursor.execute('SELECT balance FROM users WHERE username = ?', (target_username,))
+    conn.commit()
+    bal = cursor.fetchone()[0]
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=f'Баланс пользователя {target_username} теперь {bal}.')
 
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=f'Баланс пользователя {selected_user} обновлен на {new_balance}.')
-    return ConversationHandler.END
+async def send_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    if update.effective_chat.type != Chat.PRIVATE:
+        await context.bot.send_message(chat_id=update.effective_chat.id, text='Эта команда доступна только в личных сообщениях.', message_thread_id=4)
+        return
 
+    user_name = update.message.from_user.username
+    if user_name not in ['hlebnastole', 'why_dyrachyo','sdmfy']:
+        await context.bot.send_message(chat_id=update.effective_chat.id, text='У вас нет доступа к этой команде')
+        return
+    args = context.args
+    message_text = ' '.join(args)
+    target_chat_id = '-1002195401547'  # Замените на ID целевого чата
 
+    await context.bot.send_message(chat_id=target_chat_id, text=message_text, message_thread_id=4)
+    await context.bot.send_message(chat_id=update.effective_chat.id, text='Сообщение отправлено.')
 
-
-
-async def button2(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="Вы нажали Кнопку 2")
 
 def main():
     application = ApplicationBuilder().token(TOKEN).build()
-
-    conv_handler = ConversationHandler(
-        entry_points=[CommandHandler('admin', admin)],
-        states={
-            SELECT_USER: [MessageHandler(filters.TEXT & ~filters.COMMAND, select_user)],
-            ENTER_BALANCE: [MessageHandler(filters.TEXT & ~filters.COMMAND, enter_balance)],
-        },
-        fallbacks=[],
-    )
-
     application.add_handler(CommandHandler('start', start))
     application.add_handler(MessageHandler(filters.Dice.ALL, handle_dice))
     application.add_handler(CommandHandler('register', register))
-    application.add_handler(conv_handler)
-    application.add_handler(MessageHandler(filters.TEXT & filters.Regex('^Изменить баланс пользователя$'), button1))
-    application.add_handler(MessageHandler(filters.TEXT & filters.Regex('^Скрытно кого-то послать$'), button2))
+    application.add_handler(CommandHandler('balance', balance))
+    application.add_handler(CommandHandler('write', send_message))
 
     application.run_polling()
 
@@ -266,6 +250,6 @@ if __name__ == '__main__':
     main()
 
 
-if __name__ == '__main__':
-    main()
+
+
 
