9c8b7f86618a4fb0e994d946e72514a82433f97e
Dima <151432491+psibladdd@users.noreply.github.com>
2024-09-24 17:38:38 +0300

    Update main.py

--- bot_blin/main.py
+++ bot_blin/main.py
@@ -524,7 +524,7 @@ async def quiz(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         return
 
     user_name = update.message.from_user.username
-    if user_name not in ['why_dyrachyo']:
+    if user_name not in ['why_dyrachyo', 'hlebnastole']:
         await context.bot.send_message(chat_id=update.effective_chat.id, text='У вас нет доступа к этой команде')
         return
 
